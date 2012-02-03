f6f254b380bad9f86523d9d5737c17cc98977d6e
Julien Richard-Foy <julien@richard-foy.fr>
2012-02-03 09:57:07 +0100

    Embed forest

--- forest.sh
+++ forest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-FOREST=/Users/julienrichard-foy/workspace/forest/compiler/target/scala-2.9.1/forest_2.9.1-0.1-one-jar.jar
+FOREST=lib/forest_2.9.1-0.1-one-jar.jar
 
 java -jar $FOREST app app/views scala forest.backends.Play2Json
 java -jar $FOREST app public/javascripts/generated js
